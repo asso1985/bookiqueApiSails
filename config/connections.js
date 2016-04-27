6beef1521b38a0b8470338538d52dd80f6c74ee4
Omar <assadi.omar.1985@gmail.com>
2016-04-27 23:35:58 +0200

    Added local dependecy

--- config/connections.js
+++ config/connections.js
@@ -28,9 +28,9 @@ module.exports.connections = {
   * Installed by default.                                                    *
   *                                                                          *
   ***************************************************************************/
-  // localDiskDb: {
-  //   adapter: 'sails-disk'
-  // },
+  localDiskDb: {
+    adapter: 'sails-disk'
+  },
 
   /***************************************************************************
   *                                                                          *
@@ -40,13 +40,13 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER',
-  //   password: 'YOUR_MYSQL_PASSWORD',
-  //   database: 'YOUR_MYSQL_DB'
-  // },
+  someMysqlServer: {
+    adapter: 'sails-mysql',
+    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
+    user: 'YOUR_MYSQL_USER',
+    password: 'YOUR_MYSQL_PASSWORD',
+    database: 'YOUR_MYSQL_DB'
+  },
 
   /***************************************************************************
   *                                                                          *
@@ -58,14 +58,17 @@ module.exports.connections = {
   ***************************************************************************/
   bookiqueMongodbServer: {
     adapter: 'sails-mongo',
-    //host: '  mongodb://heroku_cpj7w2xl:A55ad10666@ds021771.mlab.com:21771/heroku_cpj7w2xl',
-    //host : "localhost",
-    url : process.env.MONGOLAB_URI
-    //port: 27017,
+    host: 'localhost',
+    port: 27017,
     // user: 'omar',
     // password: 'A55ad1sp',
     // database: 'your_mongo_db_name_here'
   },
+
+  // bookiqueMongodbServer: {
+  //   adapter: 'sails-mongo',
+  //   url : process.env.MONGOLAB_URI
+  // },  
 
   /***************************************************************************
   *                                                                          *
@@ -76,13 +79,13 @@ module.exports.connections = {
   *                                                                          *
   *                                                                          *
   ***************************************************************************/
-  // somePostgresqlServer: {
-  //   adapter: 'sails-postgresql',
-  //   host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_POSTGRES_USER',
-  //   password: 'YOUR_POSTGRES_PASSWORD',
-  //   database: 'YOUR_POSTGRES_DB'
-  // }
+  somePostgresqlServer: {
+    adapter: 'sails-postgresql',
+    host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
+    user: 'YOUR_POSTGRES_USER',
+    password: 'YOUR_POSTGRES_PASSWORD',
+    database: 'YOUR_POSTGRES_DB'
+  }
 
 
   /***************************************************************************
