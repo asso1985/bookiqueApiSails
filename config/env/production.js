cabdc05700ff16c44e197d65fa439720d53113ec
Omar <assadi.omar.1985@gmail.com>
2016-04-28 00:44:30 +0200

    Added local dependecy

--- config/env/production.js
+++ config/env/production.js
@@ -16,10 +16,11 @@ module.exports = {
    * Set the default database connection for models in the production        *
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
- 
-  models: {
-    connection: 'bookiqueMongodbServer'
-  }  
+
+  // models: {
+  //   connection: 'someMysqlServer'
+  // },
+  
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
